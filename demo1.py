c8cda124f8c4583c8489dbff49322e0f6a297037
125chaitanya <123243532+125chaitanya@users.noreply.github.com>
2023-09-21 12:36:42 +0530

    Add files via upload

---
+++ demo1.py
@@ -0,0 +1,11 @@
+n = int(input("Enter the value of n:"))
+if(n>0):
+    print("is positive")
+elif(n<0):
+    print(" is negative")
+    if(n==-10):
+        print("number is 0")
+    else:
+        print("number is not zero")
+else:
+    print("it is nothing")
